a4cefb23fd7cd42604c8c9018368c003ced5b5c0
z9877969 <z9877969@ukr.net>
2022-11-22 12:44:25 +0200

    adds imagine react-redux connect, ReactReduxProvider with Counter

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,28 +1,12 @@
-// import { useSelector } from "react-redux";
-import { useSelector } from "../components/App";
 import TodoFilter from "../components/TodoFilter/TodoFilter";
 import ToDoForm from "../components/TodoForm/TodoForm";
 import ToDoList from "../components/TodoList/TodoList";
-import { useLocalStorage } from "../hooks/useLocalStorage";
-import { getIsTodoExist } from "../redux/todo/todoSelectors";
 
 const TodoPage = () => {
-  const state = useSelector((state) => state);
-  console.log("state :>> ", state);
-
-  // const [filter, setFilter] = useLocalStorage("filter", "all");
-  // const isTodoExist = useSelector(getIsTodoExist);
-
-  // const handleChange = (e) => {
-  //   setFilter(e.target.value);
-  // };
-
-  console.log("TodoPage");
   return (
     <>
       <ToDoForm />
       <TodoFilter />
-      {/* {isTodoExist && <ToDoList />} */}
       <ToDoList />
     </>
   );
