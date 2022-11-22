795736f7522d45571476d27d29b518ff72035e81
z9877969 <z9877969@ukr.net>
2022-11-22 12:30:13 +0200

    adds react-redux connect, ReactReduxProvider

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,6 +1,5 @@
 import { createContext, useCallback, useContext, useState } from "react";
 
-const MainContext = createContext();
 const AddTodoContext = createContext();
 const RemoveTodoContext = createContext();
 const DataTodoContext = createContext();
@@ -9,7 +8,7 @@ export const useAddTodo = () => useContext(AddTodoContext);
 export const useRemoveTodo = () => useContext(RemoveTodoContext);
 export const useTodo = () => useContext(DataTodoContext);
 
-const MainProvider = ({ children }) => {
+const TodoProvider = ({ children }) => {
   const [todo, setTodo] = useState([]);
 
   const add = useCallback((todo) => setTodo((prev) => [...prev, todo]), []);
@@ -17,16 +16,6 @@ const MainProvider = ({ children }) => {
     (id) => setTodo((prev) => prev.filter((el) => el.id !== id)),
     []
   );
-
-  return (
-    <MainContext.Provider value={{ todo, add, remove }}>
-      {children}
-    </MainContext.Provider>
-  );
-};
-
-const ShareProvider = ({ children }) => {
-  const { todo, add, remove } = useContext(MainContext);
   return (
     <DataTodoContext.Provider value={todo}>
       <AddTodoContext.Provider value={add}>
@@ -35,14 +24,6 @@ const ShareProvider = ({ children }) => {
         </RemoveTodoContext.Provider>
       </AddTodoContext.Provider>
     </DataTodoContext.Provider>
-  );
-};
-
-const TodoProvider = ({ children }) => {
-  return (
-    <MainProvider>
-      <ShareProvider>{children}</ShareProvider>
-    </MainProvider>
   );
 };
 
